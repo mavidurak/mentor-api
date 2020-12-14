fce7f8c26f9baa55aee7551e6c4884fdc4c50e99
saracalihan <saracalihan@gmail.com>
2020-12-14 16:36:05 +0300

    yazım duzeltildi

--- src/routes/authentications/index.js
+++ src/routes/authentications/index.js
@@ -57,7 +57,7 @@ const register_validation = {
       .max(30)
       .required(),
     email: Joi.string()
-      .email({ minDomainSegments: 2}),
+      .email({ minDomainSegments: 2 }),
     name: Joi.string().min(3).max(30).required()
   })
 };
