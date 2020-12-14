5eae05f996fd82f81ee406912000fa4b6b2c9641
Alihan SARAÇ <56413673+saracalihan@users.noreply.github.com>
2020-12-14 20:29:16 +0300

    Update 20201214170020-rename-data_sets-key_title_column.js

--- migrations/20201214170020-rename-data_sets-key_title_column.js
+++ migrations/20201214170020-rename-data_sets-key_title_column.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface. renameColumn("data_sets", "key_title", "data_type");
+    return queryInterface.renameColumn("data_sets", "key_title", "data_type");
   },
   down: function (queryInterface, Sequelize) {
-    return queryInterface. renameColumn("data_sets", "data_type", "key_title");
+    return queryInterface.renameColumn("data_sets", "data_type", "key_title");
   }
 };
