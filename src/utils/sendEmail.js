b75f300b751b23dce1ef73a3eb020eec372bf7c5
saracalihan <saracalihan@gmail.com>
2020-12-28 19:59:08 +0300

    fix

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,4 +1,6 @@
 import nodemailer from 'nodemailer';
+import * as handlebars from 'handlebars';
+import * as fs from 'fs';
 
 export const EmailTypes = {
   confirm: 'confirm',
@@ -6,7 +8,7 @@ export const EmailTypes = {
   changeApplicationPermission: 'changeApplicationPermission'
 }
 
-export const sendEmail = async ( emailType, {user, token} ) => {
+export const sendEmail = async ( emailType, user, token ) => {
   
   var email={
     sender: process.env.EMAIL_USER,
@@ -15,19 +17,28 @@ export const sendEmail = async ( emailType, {user, token} ) => {
     content: null,
     buttonHref: null
   }
-
+  var source, template, replacements
   switch (emailType) {
     case 'confirm':
       email.subject='Welcome to MaviDurak-IO'
-      email.buttonHref = `${process.env.API_PATH}/authentications/email-confirmation?token=${token}`;
-      /*
-        Template okuyan, kullanıcıya gore yeniden duzenleyen ve email.content e atayan kod parçası
-      */
+      email.buttonHref = `${process.env.API_PATH}/authentications/email-confirmation?token=${token}`;  
+      source = fs.readFileSync('src/templates/ConfirmationEmail.html', 'utf-8').toString();
+      template = handlebars.compile(source);
+      replacements = {
+        username: user.name,
+        href: email.buttonHref
+      };
       break;
 
     case 'forgotPassword':
       email.subject='Reset your MaviDurak-IO account password'
       email.buttonHref = `${process.env.API_PATH}/authentications/forgot-password?token=${token}`;
+      source = fs.readFileSync('src/templates/ForgotPasswordEmail.html', 'utf-8').toString();
+      template = handlebars.compile(source);
+      replacements = {
+        username: user.name,
+        href: email.buttonHref
+      };  
     break;
 
     case 'changeApplicationPermission':
@@ -49,14 +60,15 @@ export const sendEmail = async ( emailType, {user, token} ) => {
     },
   });
 
+  email.content = template(replacements);  
   // send mail with defined transport object
   const info = await transporter.sendMail({
     from: email.from,
     to: email.to,
     subject: email.subject,
-    html: email.content,
+    html: email.content
   });
-
+  
   console.log(`\n++++++++++++++++| ${emailType} Message sent to ${email.to} - ${info.messageId} |++++++++++++++++\n`);
 };
 
