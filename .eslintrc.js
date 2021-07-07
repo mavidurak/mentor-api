aabfdc6478df638f089968b138dcd7f789ee5190
Alihan SARAÇ(Brick) <56413673+saracalihan@users.noreply.github.com>
2021-07-07 19:53:55 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     camelcase: 'off',
   },
 };
