c7c0f9f643848476b44e7e155a21e2b031e51e0f
DEONSKY <bestrasen@hotmail.com>
2021-10-08 18:29:40 +0300

    hotfix

--- src/models/application.js
+++ src/models/application.js
@@ -56,14 +56,6 @@ function createToken() {
 
 const initialize = (models) => {
 
-  models.applications.belongsTo(models.data_sets, {
-    as: 'data_sets',
-    foreignKey: {
-      name: 'dataset_id',
-      allowNull: false,
-    },
-  });
-
   models.applications.hasMany(
     models.locations, {
       as: 'locations',
