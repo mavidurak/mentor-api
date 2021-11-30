73810538fd454b6b6fcf3dd3d1e8f8b6368d6422
saracalihan <saracalihan@gmail.com>
2021-11-30 18:45:11 +0300

    Custom HTTPError added

--- src/pre_handlers/index.js
+++ src/pre_handlers/index.js
@@ -1,7 +1,9 @@
 import fixPath from './fixPath';
+import errorHandler from '../handlers/errorHandler';
 import authentication from './authentication';
 
 export default [
   fixPath,
+  errorHandler,
   authentication,
 ];
