5745ad0ae884a4859be3195a64a9a15bfcada212
burakack <burak_ack@hotmail.com>
2021-12-20 21:14:25 +0300

    istenilenler

--- migrations/20211205122532-add-status-to-applications.js
+++ migrations/20211205122532-add-status-to-applications.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     queryInterface.addColumn('applications', 'is_alive', {
-      type: Sequelize.STRING,
+      type: Sequelize.BOOLEAN,
       defaultValue: false,
     });
   },
